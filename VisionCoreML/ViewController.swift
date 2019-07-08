0ed3199c311fb895a4d74ff424f0921e67e610b9
Koji4104 <koji4104@gmail.com>
2019-07-08 22:32:31 +0900

    Initial Commit

---
+++ VisionCoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionCoreML
+//
+//  Created by Koji4104 on 2019/07/08.
+//  Copyright © 2019 Koji4104. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
